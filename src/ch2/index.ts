bd2c1cb8c980a4f75d88c7ba9c34d96f83607bff
AStratienko <artem.stratienko@ext.us.panasonic.com>
2021-03-03 17:32:55 +0200

    tasks from ch 2

---
+++ src/ch2/index.ts
@@ -0,0 +1,40 @@
+export const triangle = ():void => {
+    let emptyString: string = "";
+
+    for(let i = 0; i < 7; i++){
+        emptyString += "#"
+        console.log(emptyString)
+    }
+};
+
+export const fizzBuzz = ():void => {
+    for(let i = 1; i <= 50; i++){
+
+        let result: string = "";
+
+        if( i % 3 === 0 ){
+            result =  result.concat("Fizz")
+
+        }
+        if( i % 5 === 0 ){
+            result =  result.concat("Buzz")
+            
+        }
+        
+        console.log(result || i)
+    }
+}
+
+export const chessDesk = (size: number): string =>{
+    let board: string = "";
+
+for(let i = 0; i< size; i++){
+    board = i % 2 === 0 
+    ? board.concat(`${" #".repeat(size / 2)}\n`)
+    : board.concat(`${"# ".repeat(size / 2)}\n`)
+}
+    return board;
+}
+
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("hello world");
+import { triangle, fizzBuzz, chessDesk } from './ch2/index';
+// triangle()
+
+// fizzBuzz();
+
+// console.log(chessDesk(8));
+
